5ca62dd6769bfbf1376ebebcbda3c1d519e9cb8b
Stephen Daniel <stephen.daniel@students.plymouth.ac.uk>
2020-07-21 11:34:46 +0100

    Web page Navigation bar is set. Server details are set. both placed in a library folder

---
+++ code/library/navbar.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      
+    </title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	
+	
+	
+	
+    <link rel="stylesheet" href="public/3c-theme.css">
+    
+  </head>
+  <body>
+   
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">Family Organiser</a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav"> 
+  
+	<li class="nav-item">
+      <a class="nav-link" href="index 2.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="3b-products.php">Parent</a>
+    </li>
+   
+    <li class="nav-item">
+      <a class="nav-link" href="menu.php">Child</a>
+    </li>
+	<li class="nav-item">
+      <a class="nav-link" href="login 2.php">new Parent</a>
+    </li>
+	<li class="nav-item">
+      <a class="nav-link" href="register.php">new Child</a>
+    </li>
+	<li class="nav-item">
+      <a class="nav-link" href="..//web/index.php">Home</a>
+    </li>
+	
+  </ul>
+</nav>
+
+</body>
+</html>
+
